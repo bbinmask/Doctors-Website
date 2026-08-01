3d85e725dd48d839ed541872a8bc98f74b2593ab
bbinmask <bbinmask@gmail.com>
2026-08-01 22:05:43 +0530

    Commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // Backend url
 export const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:5000";
+  (import.meta.env.VITE_BASE_URL || "http://localhost:5000") + "/api/v1";
 export const token = localStorage.getItem("token");
